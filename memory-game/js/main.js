ee4e11e10fb196543d88f5c505f844b0b188bdfb
life2free <thunderous_0518@hotmail.com>
2020-02-09 16:48:37 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,18 @@
 console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if(cardsInPlay[0]===cardsInPlay[1]){
+	alert("You found a match!");
+}else{
+	alert("Sorry, try again.");
+}
