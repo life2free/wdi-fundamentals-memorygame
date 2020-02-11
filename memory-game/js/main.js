5a4b48349a67359d3959367a8ac7b697973e3e5e
life2free <thunderous_0518@hotmail.com>
2020-02-10 20:13:20 -0500

    add flipcard logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -24,20 +24,38 @@ const cards = [
 const cardsInPlay = [];
 
 function checkForMatch(){
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-	  alert("You found a match!");
-	} else {
-	  alert("Sorry, try again.");
+	if(cardsInPlay.length>1){
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+		  alert("You found a match!");
+		} else {
+		  alert("Sorry, try again.");
+		}
 	}
 }
 
-function flipCard(cardId){
+function flipCard(){
+	let cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src',cards[cardId].cardImage);
+	checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
-checkForMatch();
+
+
+function createBoard (){
+
+	const gameBoard = document.getElementById('game-board');
+	for (let i = 0; i < cards.length; i++) {
+    	// Logic here
+    	let cardElement = document.createElement('img');
+    	cardElement.setAttribute('src',"images/back.png");
+    	cardElement.setAttribute('data-id',i);
+    	cardElement.addEventListener('click',flipCard);
+    	gameBoard.appendChild(cardElement);
+	}
+}
+
+createBoard();
